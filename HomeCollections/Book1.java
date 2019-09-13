3bbde65593a2b7e2fba078d1725f8e71ff2278ee
PavelDaletski <46653749+PavelDaletski@users.noreply.github.com>
2019-09-13 23:38:10 +0300

    Update Book1.java

--- HomeCollections/Book1.java
+++ HomeCollections/Book1.java
@@ -3,31 +3,51 @@ package HomeCollections;
 import java.util.Objects;
 
 public class Book1 {
-    private String name;
-    private String author;
+    private int id;
+    private String title;
+    private String outher;
+    private String fristname;
 
     public Book1() {
     }
 
-    public Book1(String name, String author) {
-        this.name = name;
-        this.author = author;
+    public Book1(int id, String title, String outher, String fristname) {
+        this.id = id;
+        this.title = title;
+        this.outher = outher;
+        this.fristname = fristname;
     }
 
-    public String getName() {
-        return name;
+    public int getId() {
+        return id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public String getAuthor() {
-        return author;
+    public String getTitle() {
+        return title;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOuther() {
+        return outher;
+    }
+
+    public void setOuther(String outher) {
+        this.outher = outher;
+    }
+
+    public String getFristname() {
+        return fristname;
+    }
+
+    public void setFristname(String fristname) {
+        this.fristname = fristname;
     }
 
     @Override
@@ -35,20 +55,25 @@ public class Book1 {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Book1 book1 = (Book1) o;
-        return Objects.equals(name, book1.name) &&
-                Objects.equals(author, book1.author);
+        return id == book1.id &&
+                Objects.equals(title, book1.title) &&
+                Objects.equals(outher, book1.outher) &&
+                Objects.equals(fristname, book1.fristname);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, author);
+        return Objects.hash(id, title, outher, fristname);
     }
 
     @Override
     public String toString() {
         return "Book1{" +
-                "name='" + name + '\'' +
-                ", author='" + author + '\'' +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", outher='" + outher + '\'' +
+                ", fristname='" + fristname + '\'' +
                 '}';
     }
 }
+
